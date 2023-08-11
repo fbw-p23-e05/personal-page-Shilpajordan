aced9c4187f2160a339c5c2b918677c6412bd02f
Shilpajordan <Shilpa.nair@dci-student.org>
2023-08-11 13:11:48 +0200

    modified the contact section

--- Script/scripts.js
+++ Script/scripts.js
@@ -1,2 +1,17 @@
 
-/* alert('Click me to go to this page') */
+alert('Click me to go to this page') 
+
+function validateEmail(email) {
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (email.value.trim() === "") {
+      alert("Please provide an email address.");
+      email.focus();
+      return false;
+    } else if (email.value.match(validRegex)) {
+      return true;
+    } else {
+      alert("Email address is invalid.");
+      email.focus();
+      return false;
+    }
+  }
